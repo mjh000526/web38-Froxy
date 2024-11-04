c74b8ad24b28473807114976467f8cad86aa1b24
ATeals <ateals@icloud.com>
2024-11-04 10:30:18 +0900

    chore: 기본 세팅

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { A, App, B } from './App.tsx';
+import { App } from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <A />
-    <B />
-  </StrictMode>,
+  </StrictMode>
 );
