bc509e106c3e860ffbddc6cd59cae4bac09f66f8
naarang <93020785+naarang@users.noreply.github.com>
2024-11-12 16:16:34 +0900

    Feature #21 : lotus 검색창 구현하기 (#31)
    * feat(#15):  eslint test
    
    * feat(#15): lotusCard 컴포넌트 구현하기
    
    - revert로 인해 다시 구현 내용 반영함
    - api 명세서를 바탕으로 interface 다시 반영함
    
    * feat(#15): lotusCard 컴포넌트 태그 구현하기
    
    - badge 컴포넌트 이용해서 tagList 보여주기
    - Text 컴포넌트 적용해서 통일하기
    
    * feat(#21): lotusSearchBar 컴포넌트 구현하기
    
    - Input 컴포넌트 적용하기
    - 대략적인 레이아웃 구현하기
    
    * feat(#21): lotus 검색창에 필요한 검색하는 함수 구현하기
    
    - 추가로 border 컬러 더 밝게 변경하기
    
    * fix(#21): pr 피드백 반영하기
    
    - 회색 텍스트 색상은 Muted로 통일하기
    - LotusSearchBar는 CCP 대신에 Hook을 사용하기
    - set 함수를 직접 전달하지 않고 감싸서 전달하ㅣ

--- apps/frontend/src/page/lotus.lazy.tsx
+++ apps/frontend/src/page/lotus.lazy.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@froxy/design/components';
 import { createLazyFileRoute } from '@tanstack/react-router';
 import { LotusCardList } from '@/widget/LotusList/LotusCardList';
+import { LotusSearchBar } from '@/widget/LotusList/LotusSearchBar';
 
 export const Route = createLazyFileRoute('/lotus')({
   component: RouteComponent
@@ -8,9 +9,10 @@ export const Route = createLazyFileRoute('/lotus')({
 
 function RouteComponent() {
   return (
-    <div>
+    <div className="flex flex-col gap-4 px-5">
       <Button onClick={() => alert('Lotus')}>Lotus</Button>
       <h1 className="text-red-300">Lotus world</h1>
+      <LotusSearchBar />
       <LotusCardList />
     </div>
   );
--- packages/design/src/components/index.tsx
+++ packages/design/src/components/index.tsx
@@ -1,6 +1,7 @@
 export * from './ui/badge';
 export * from './ui/button';
 export * from './ui/carousel';
+export * from './ui/input';
 export * from './ui/typography';
 export * from './ui/tabs';
 export * from './Slot';
