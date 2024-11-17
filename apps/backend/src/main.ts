168a332bdebbcc691c623fd05fa63edf73109d86
mjh000526 <50985929+mjh000526@users.noreply.github.com>
2024-11-17 17:59:28 +0900

    feat(#69): swagger apply (#70)

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,20 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: INestApplication = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Froxy swagger')
+    .setDescription('Froxy API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
   app.setGlobalPrefix('api');
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
