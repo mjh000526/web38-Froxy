022aaf605dbb99de3e135105a58d2bf9ea8ddd11
ATeals <125727432+ATeals@users.noreply.github.com>
2024-11-20 11:01:19 +0900

    Feature #94 fe cicd (#95)
    * refactor(#94):  apps/frontend 패키지 type alias에 packages/design 추가
    
    * refactor(#94):  baseurl
    
    * chore(#94):  fe-cd action
    
    * chore(#94):  not build packages
    
    - 빌드 안한 파일로 테스트
    
    * chore(#94):  오브젝트 스토리지로 업로드 명령어 수정

--- packages/react-markdown/vite.config.ts
+++ packages/react-markdown/vite.config.ts
@@ -3,7 +3,6 @@
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
@@ -19,37 +18,5 @@ export default defineConfig({
       '@': resolve(__dirname, './src')
     }
   },
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: '@froxy/react-markdown',
-      formats: ['es'], // 포맷 추가
-      fileName: (format) => `index.${format}.js` // 포맷별 파일명
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        },
-        exports: 'named', // named exports 설정
-        chunkFileNames: 'chunks/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
-        entryFileNames: '[name].js',
-        manualChunks: undefined,
-        inlineDynamicImports: false
-      }
-    }
-  },
-
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    dts({
-      tsconfigPath: './tsconfig.app.json',
-      include: ['src'],
-      rollupTypes: true // 타입 번들링
-    })
-  ]
+  plugins: [react(), tsconfigPaths()]
 });
